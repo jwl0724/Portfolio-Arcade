8336096dc3e1b9438b75c04e89a069a165d44422
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-05 16:16:51 -0800

    create enum for model file paths

---
+++ modelPaths.js
@@ -0,0 +1,29 @@
+// Model path enum
+const modelPaths = Object.freeze({
+    // Structures
+    FLOOR: "/models/floor.glb",
+    WINDOW: "/models/wall-window.glb",
+    WALL: "/models/wall.glb",
+    WALL_CORNER: "/models/wall-corner.glb",
+    DOOR: "/models/wall-door-rotate.glb",
+    COLUMN: "/models/column.glb",
+
+    // Decor
+    AIR_HOCKEY: "/models/air-hockey.glb",
+    ARCADE_MACHINE: "/models/arcade-machine.glb",
+    BASKETBALL: "/models/basketball-game.glb",
+    CASH_REGISTER: "/models/cash-register.glb",
+    CLAW_MACHINE: "/models/claw-machine.glb",
+    DANCE_MACHINE: "/models/dance-machine.glb",
+    GAMBLING_MACHINE: "/models/gambling-machine.glb",
+    PINBALL: "/models/pinball.glb",
+    PRIZE_WHEEL: "/models/prize-wheel.glb",
+    TICKET_MACHINE: "/models/ticket-machine",
+    VENDING_MACHINE: "/models/vending-machine.glb",
+
+    // Characters
+    EMPLOYEE: "/models/character-employee.glb",
+    PLAYER: "/models/character-gamer.glb",
+});
+
+export { modelPaths };
