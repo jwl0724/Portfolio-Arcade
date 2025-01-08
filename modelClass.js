12026eb5a9216f326a8a2d6eba1e8bb64d731221
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-01-07 16:02:20 -0800

    create prototypical model class

---
+++ modelClass.js
@@ -0,0 +1,29 @@
+import modelPaths from "/modelPaths.js";
+
+export { Model };
+
+// TODO: Implement class
+class Model {
+    // Data members
+    #animations;
+    #scene;
+
+    constructor(modelFilePath) {
+        // TODO: move loading stuff here
+    }
+
+    // TODO: will move character to the new position vector
+    move(newPositionVector) {
+
+    }
+
+    // TODO: will rotate the model to the direction of the new position vector
+    rotate(newPositionVector) {
+
+    }
+
+    // TODO: will play the animation with the given name
+    playAnimation(animationName) {
+        
+    }
+}
