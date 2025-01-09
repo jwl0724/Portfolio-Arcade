4d198e86e00c9e8675425fbc5c5800c9dbb7e458
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-08 17:04:23 -0800

    create script to interface with all the arcade managing classes

---
+++ script.js
@@ -0,0 +1,6 @@
+// TODO: Implement some button or something to determine if load arcade or not
+// For now just load the arcade directly since that's the focus
+import { Arcade } from "./classes/arcadeClass";
+
+const arcade = new Arcade();
+await arcade.buildArcade();
