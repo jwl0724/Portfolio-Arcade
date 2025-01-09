29bc4d2c98d1a29f25786d1fc77fc3339f9bf427
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-08 17:04:34 -0800

    create class to manage camera movements

---
+++ classes/cameraManagerClass.js
@@ -0,0 +1,19 @@
+import * as THREE from "three";
+
+export { CameraManager };
+
+class CameraManager {
+    
+    #camera;
+
+    constructor(fov, aspect, near, far) {
+        this.#camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        this.#camera.position.set(5, 3, 2);
+        this.#camera.lookAt(5, 0, 2);
+        this.#camera.rotateX(Math.PI / 3);
+    }
+
+    getCamera() {
+        return this.#camera;
+    }
+}
