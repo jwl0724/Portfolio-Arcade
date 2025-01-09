8d06d8a5cfe200ac906e9d3324a6a4fa19687647
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-01-09 15:34:16 -0800

    add player movement using refactored code

--- classes/playerClass.js
+++ classes/playerClass.js
@@ -1,21 +1,49 @@
 import * as THREE from 'three';
 import { CharacterModel } from './characterModelClass';
 import { ModelPaths } from '../modelPaths';
+import { InputManager } from './inputManagerClass';
 
 export { Player };
 
 class Player {
     // Data members
     #isMoving = false;
-    #moveSpeed = 0.1;
+    #inputManager;
+    #moveSpeed = 1.75;
     #position = new THREE.Vector3(0, 0, 0);
     #rotation = 0; // Only rotate around the y-axis
     #directionVector = new THREE.Vector3(0, 0, 0);
     #modelClass;
     
     constructor(positionVector, rotationAngle) {
+        this.#inputManager = new InputManager(this);
         this.#position = positionVector;
         this.#rotation = rotationAngle;
+    }
+
+    // PROCESS SECTION: Put code that needs to run per frame in these parts
+    playerPhysicsProcess(delta) {
+        this.#updateDirectionVector();
+        this.#updatePosition(delta);
+    }
+
+    playerProcess(delta) {
+
+    }
+
+    playerInputProcess() {
+
+    }
+
+    #updateDirectionVector() {
+        const inputVector = this.#inputManager.getInputVector();
+        this.#directionVector = inputVector.normalize();
+    }
+
+    #updatePosition(delta) {
+        const nextPoint = this.#directionVector.multiplyScalar(this.#moveSpeed * delta);
+        this.#position = this.#position.add(nextPoint);
+        this.#modelClass.updateModel(this.#position);
     }
 
     async createPlayer(arcadeScene, mixerCollection) {
@@ -25,15 +53,4 @@ class Player {
         this.#modelClass.playAnimation(CharacterModel.ANIMATION_NAMES.IDLE, THREE.LoopRepeat);
         this.#modelClass.setPosition(this.#position.x, this.#position.y, this.#position.z);
     }
-
-    // Input direction will be wherever the screen is clicked OR keyboard input direction
-    updateDirectionVector(inputDirection) {
-        this.#directionVector = inputDirection.normalized();
-    }
-
-    // Will move the player in the direction of the direction vector
-    updatePosition(delta) {
-        this.#modelClass.updatePosition(this.#directionVector.multiplyScalar(this.#moveSpeed * delta));
-    }
-
 }
