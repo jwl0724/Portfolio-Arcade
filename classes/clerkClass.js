14c7d6c7deda181019099ee04f064a30c0888380
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-14 08:45:50 -0800

    add basic interact functionality

--- classes/clerkClass.js
+++ classes/clerkClass.js
@@ -7,29 +7,48 @@ export { Clerk };
 
 class Clerk {
     
+    #ready = false;
     #characterModel;
     #isInteracting;
+    #inputManager;
+    #interactBox;
     #position;
 
-    constructor(position) {
+    constructor(position, inputManager) {
         this.#position = position;
-    }
-    
-    clerkProcess(delta) {
-
-    }
-
-    clerkInputProcess() {
-        if (this.#isInteracting)
+        this.#inputManager = inputManager;
     }
 
     async createClerk(scene, mixers) {
+        // Load clerk model into scene
         this.#characterModel = new CharacterModel(ModelPaths.EMPLOYEE);
         await this.#characterModel.loadModel(scene, mixers);
         this.#characterModel.setPosition(this.#position.x, this.#position.y, this.#position.z);
+
+        // Create interact box for clerk
+        this.#interactBox = new THREE.Box3(
+            new THREE.Vector3(this.#position.x - 1, this.#position.y - 1, this.#position.z - 1.25), 
+            new THREE.Vector3(this.#position.x + 1, this.#position.y + 1, this.#position.z + 1.25)
+        );
+        const helper = new THREE.Box3Helper(this.#interactBox, 0xffff00);
+        scene.add(helper);
+
+        // Mark class as ready
+        this.#ready = true;
     }
 
-    interact() {
+    onInteract(player) {
+        if (!this.#ready || this.#isInteracting) return;
+        
+        // Check if player is in interact range
+        if (!this.#interactBox.containsPoint(player.getModel().position)) return;
 
+        // Check if player is facing towards clerk
+
+        console.log("interacted!");
+    }
+
+    stopInteraction() {
+        this.#isInteracting = false;
     }
 }
