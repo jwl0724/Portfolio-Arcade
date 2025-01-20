0584f959402a8b77b668c01be52062d248590c43
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-01-20 14:44:40 -0800

    random commit bc i gotta do other stuff now

--- classes/dialogueManagerClass.js
+++ classes/dialogueManagerClass.js
@@ -9,52 +9,67 @@ const TEXTBOX_COLOR = 0xdadee6;
 
 class DialogueManager {
 
-    #textbox;
-    #textCanvas;
+    // Components
     #arcadeScene;
-    #displayedText;
-    #responseBoxes;
+    #chatPromptBox;
+    #chatPromptText;
+    #chatPromptCanvas;
+
+    // Running variables
+    #animating = false;
 
     constructor(scene) {
         this.#arcadeScene = scene;
-        this.#responseBoxes = new Array();
-
-        // Create text Texture
-        this.#textCanvas = document.createElement("canvas");
-        this.#displayedText = "send help";
         
-        // Create text box
-        const geometry = new THREE.PlaneGeometry(0.4, 0.1);
-        const textTexture = new THREE.Texture(this.#textCanvas);
-        textTexture.needsUpdate = true;
-        textTexture.minFilter= THREE.NearestFilter;
-        textTexture.magFilter = THREE.NearestFilter;
+        // Create ellipses text
+        this.#chatPromptCanvas = document.createElement("canvas");
+        this.#chatPromptText = "...";
+        const textTexture = new THREE.Texture();
+        
 
-        const material = new THREE.MeshBasicMaterial({ color: TEXTBOX_COLOR, side: THREE.DoubleSide, map: textTexture });
-        this.#textbox = new THREE.Mesh(geometry, material);
-        this.#textbox.position.set(0, -20, 0); // Hide the text box somewhere else
-        this.#arcadeScene.add(this.#textbox);
+        // Create chat prompt box
+        const geometry = new THREE.PlaneGeometry(1.4, 1);
+        
+        
+
+        // this.#responseBoxes = new Array();
+
+        // // Create text Texture
+        // this.#textCanvas = document.createElement("canvas");
+        // this.#displayedText = "send help";
+        
+        // // Create text box
+        // const geometry = new THREE.PlaneGeometry(0.4, 0.1);
+        // const textTexture = new THREE.Texture(this.#textCanvas);
+        // textTexture.needsUpdate = true;
+        // textTexture.minFilter= THREE.NearestFilter;
+        // textTexture.magFilter = THREE.NearestFilter;
+
+        // const material = new THREE.MeshBasicMaterial({ color: TEXTBOX_COLOR, side: THREE.DoubleSide, map: textTexture });
+        // this.#textbox = new THREE.Mesh(geometry, material);
+        // this.#textbox.position.set(0, -20, 0); // Hide the text box somewhere else
+        // this.#arcadeScene.add(this.#textbox);
     }
 
     dialogueProcess(delta) {
-        if (!this.#displayedText) return;
-        const texture = this.#textCanvas.getContext("2d");
+        // if (!this.#displayedText) return;
+        // const texture = this.#textCanvas.getContext("2d");
 
-        texture.font = "20pt Arial";
-        texture.fillStyle = "white";
-        texture.fillRect(0, 0, this.#textCanvas.width, this.#textCanvas.height);
-        texture.fillStyle = "black";
-        texture.textAlign = "center";
-        texture.textBaseline = "middle";
-        texture.fillText(this.#displayedText, this.#textCanvas.width / 2, this.#textCanvas.height / 2);
+        // texture.font = "20pt Arial";
+        // texture.fillStyle = "white";
+        // texture.fillRect(0, 0, this.#textCanvas.width, this.#textCanvas.height);
+        // texture.fillStyle = "black";
+        // texture.textAlign = "center";
+        // texture.textBaseline = "middle";
+        // texture.fillText(this.#displayedText, this.#textCanvas.width / 2, this.#textCanvas.height / 2);
     }
 
     startDialogue(camera) {
-        this.#textbox.position.set(camera.position.x, camera.position.y - 0.1, camera.position.z - 0.275);
+        // this.#textbox.position.set(camera.position.x, camera.position.y - 0.1, camera.position.z - 0.275);
     }
     
     exitDialogue() {
-        this.#textbox.position.y -= 20; // Hide the text box somewhere
+        // this.#textbox.position.y -= 20; // Hide the text box somewhere
     }
 
     #displayText(text) {
