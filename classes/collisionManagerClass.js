fef9e7d27cff65a9b7b8f12e3f71c79fdc12349b
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-02-11 07:03:58 -0800

    add usage of player hitbox

--- classes/collisionManagerClass.js
+++ classes/collisionManagerClass.js
@@ -4,6 +4,9 @@ export { CollisionManager };
 
 class CollisionManager {
 
+    // Static constants
+    static collisionThreshold = 0.05;
+
     #environmentHitboxes;
     #playerClass;
 
@@ -12,15 +15,27 @@ class CollisionManager {
     }
 
     collisionProcess(delta) {
-        let collision = false;
-        const playerNextPosition = this.#playerClass.getNextFramePosition(delta);
-        for(let i = 0; i < this.#environmentHitboxes.length; i++) {
-            if (this.#environmentHitboxes[i].containsPoint(playerNextPosition)) {
-                this.#playerClass.notifyCollision(this.#environmentHitboxes[i]);
-                collision = true;
-            }
-        }
-        if (!collision) this.#playerClass.notifyCollision(null);
+        if (!this.#playerClass.getHitbox()) return; // Player hitbox loads one frame after, needs check to prevent premature running
+
+        // Test if collision in next frame
+        const currentPosition = this.#playerClass.getPosition();
+        this.#playerClass.setPosition(this.#playerClass.getNextFramePosition(delta));
+        const playerHitbox = this.#playerClass.getHitbox();        
+        this.#environmentHitboxes.forEach(hitbox => {
+            if (hitbox.intersectsBox(playerHitbox)) this.#playerClass.notifyCollision(hitbox);
+        });
+        // Put player back to original position
+        this.#playerClass.setPosition(currentPosition);
+
+        // let collision = false;
+        // const playerNextPosition = this.#playerClass.getNextFramePosition(delta);
+        // for(let i = 0; i < this.#environmentHitboxes.length; i++) {
+        //     if (this.#environmentHitboxes[i].containsPoint(playerNextPosition)) {
+        //         this.#playerClass.notifyCollision(this.#environmentHitboxes[i]);
+        //         collision = true;
+        //     }
+        // }
+        // if (!collision) this.#playerClass.notifyCollision(null);
     }
 
     addPlayerClass(playerClass) {
