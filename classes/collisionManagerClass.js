f80a11daa1a0196aaf5305dd9d6221cc0f501e44
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-02-14 15:31:32 -0800

    remove unused code

--- classes/collisionManagerClass.js
+++ classes/collisionManagerClass.js
@@ -17,25 +17,15 @@ class CollisionManager {
     collisionProcess(delta) {
         if (!this.#playerClass.getHitbox()) return; // Player hitbox loads one frame after, needs check to prevent premature running
 
-        // Test if collision in next frame
+        // Test if collision in next frame (prevents jitterring when colliding)
         const currentPosition = this.#playerClass.getPosition();
         this.#playerClass.setPosition(this.#playerClass.getNextFramePosition(delta));
-        const playerHitbox = this.#playerClass.getHitbox();        
+        const playerHitbox = this.#playerClass.getHitbox();
         this.#environmentHitboxes.forEach(hitbox => {
             if (hitbox.intersectsBox(playerHitbox)) this.#playerClass.notifyCollision(hitbox);
         });
         // Put player back to original position
         this.#playerClass.setPosition(currentPosition);
-
-        // let collision = false;
-        // const playerNextPosition = this.#playerClass.getNextFramePosition(delta);
-        // for(let i = 0; i < this.#environmentHitboxes.length; i++) {
-        //     if (this.#environmentHitboxes[i].containsPoint(playerNextPosition)) {
-        //         this.#playerClass.notifyCollision(this.#environmentHitboxes[i]);
-        //         collision = true;
-        //     }
-        // }
-        // if (!collision) this.#playerClass.notifyCollision(null);
     }
 
     addPlayerClass(playerClass) {
