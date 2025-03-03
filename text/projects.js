3f9ac1701644859c5a6b3a10b846868bd418a14e
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-03 14:51:02 -0800

    add duck hunt at home information

--- text/projects.js
+++ text/projects.js
@@ -12,6 +12,20 @@ const TERRAIN_GENERATOR_SIMULATOR = Object.freeze({
     ]
 });
 
+const DUCK_HUNT_AT_HOME = Object.freeze({
+    TITLE: "Duck Hunt at Home",
+    ABOUT: `Duck Hunt at Home was a game project I created to learn Godot in a 3D context. The game is an endless arena shooter where enemies are constantly respawning, and you must eliminate the ducks to earn points.
+
+    The game is loosely inspired by the old Nintendo series Duck Hunt where you shoot ducks flying across the screen. In this re-imagining, the ducks are coming are coming after you and you must survive for as long as possible.
+
+    Through creating this project, it helped solidify my foundations in working within a 3D environment, as well as improving my code architecturing. I've also learned from some of the shortcomings from this project, such as the lack of object pooling, which is the cause of the game's performannce issues.
+    `,
+    CREDITS: [
+        "The project was made using open source Godot 4.2.2 Game Engine",
+        // TODO: Look at the credits later
+    ]
+});
+
 const Projects = Object.freeze({
     TERRAIN_GENERATOR_SIMULATOR: TERRAIN_GENERATOR_SIMULATOR
 });
