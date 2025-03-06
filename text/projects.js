14f8190d8fe5df469fed4d7199ace665b4dd0a52
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-03-06 03:22:02 -0800

    remove credits section and add images section

--- text/projects.js
+++ text/projects.js
@@ -5,10 +5,8 @@ const TERRAIN_GENERATOR_SIMULATOR = Object.freeze({
     ABOUT: `This project was made as a proof of concept for a larger idea for a mobile game, one which every player has their own randomly generated island they can customize themselves, similar to that of Animal Crossing New Horizons.
 
     The island was generated using Perlin noise maps, which determined the elevation of each tile on the island. Each time the generate button is pressed a new noise map is used to create the island. Currently each tile is completely random, future considerations is to use noise maps to determine an area's "biome", where the appropriate tiles can then be selected based on the biome type.`,
-    CREDITS: [
-        "Tiles were made by Kenney's free game assets (Patreon: https://www.patreon.com/kenney)",
-        "The project was made using the open source Godot 4.2.2 Game Engine",
-        "Water shader made by paddy-exe found on Godot Shaders"
+    IMAGES: [
+        "../images/previews/terrainGeneratorSimulator/demo.gif"
     ]
 });
 
@@ -20,9 +18,12 @@ const DUCK_HUNT_AT_HOME = Object.freeze({
 
     Through creating this project, it helped solidify my foundations in working within a 3D environment, as well as improving my code architecturing. I've also learned from some of the shortcomings from this project, such as the lack of object pooling, which is the cause of the game's performannce issues.
     `,
-    CREDITS: [
-        "The project was made using open source Godot 4.2.2 Game Engine",
-        // TODO: Look at the credits later
+    IMAGES: [
+        "../images/previews/duckHuntAtHome/city.gif",
+        "../images/previews/duckHuntAtHome/plains.gif",
+        "../images/previews/duckHuntAtHome/space.gif",
+        "../images/previews/duckHuntAtHome/levels.gif",
+        "../images/previews/duckHuntAtHome/menu.gif"
     ]
 });
 
@@ -31,7 +32,10 @@ const PAWSITIVE = Object.freeze({
     ABOUT: `PAWsitive was a hackathon submission for BCIT's QDS 2024 Hackathon, where the project was the <b>first place winner</b>. The project is a proof of concept towards a mobile app that rewarded players with in-game currency whenever player's did any healthy activities, this includes exercise and meditation. After obtaining the currency, players can use it to pull for cute outfits and pets that they can decorate their avatar with.
 
     The initial concept came from the idea that "gacha games" tend to have addictive elements to them that keeps their players hooked. We thought instead of using those addictive elements to exploit players, we should instead use them to build healthy habits in our players.
-    `
+    `,
+    IMAGES: [
+        "../images/previews/PAWsitive/homepage.png"
+    ]
 });
 
 const Projects = Object.freeze({
