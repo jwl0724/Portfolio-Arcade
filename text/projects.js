d43516d0dff74d86c03cf153b68329fb23702e7b
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-06 12:58:41 -0800

    fix loss of whitespace formatting issues

--- text/projects.js
+++ text/projects.js
@@ -2,7 +2,7 @@ export { Projects }
 
 const TERRAIN_GENERATOR_SIMULATOR = Object.freeze({
     TITLE: "Terrain Generator Simulator",
-    ABOUT: `This project was made as a proof of concept for a larger idea for a mobile game, one which every player has their own randomly generated island they can customize themselves, similar to that of Animal Crossing New Horizons.
+    ABOUT: `    This project was made as a proof of concept for a larger idea for a mobile game, one which every player has their own randomly generated island they can customize themselves, similar to that of Animal Crossing New Horizons.
 
     The island was generated using Perlin noise maps, which determined the elevation of each tile on the island. Each time the generate button is pressed a new noise map is used to create the island. Currently each tile is completely random, future considerations is to use noise maps to determine an area's "biome", where the appropriate tiles can then be selected based on the biome type.`,
     IMAGES: [
@@ -12,7 +12,7 @@ const TERRAIN_GENERATOR_SIMULATOR = Object.freeze({
 
 const DUCK_HUNT_AT_HOME = Object.freeze({
     TITLE: "Duck Hunt at Home",
-    ABOUT: `Duck Hunt at Home was a game project I created to learn Godot in a 3D context. The game is an endless arena shooter where enemies are constantly respawning, and you must eliminate the ducks to earn points.
+    ABOUT: `    Duck Hunt at Home was a game project I created to learn Godot in a 3D context. The game is an endless arena shooter where enemies are constantly respawning, and you must eliminate the ducks to earn points.
 
     The game is loosely inspired by the old Nintendo series Duck Hunt where you shoot ducks flying across the screen. In this re-imagining, the ducks are coming are coming after you and you must survive for as long as possible.
 
@@ -29,7 +29,7 @@ const DUCK_HUNT_AT_HOME = Object.freeze({
 
 const PAWSITIVE = Object.freeze({
     TITLE: "PAWsitive",
-    ABOUT: `PAWsitive was a hackathon submission for BCIT's QDS 2024 Hackathon, where the project was the <b>first place winner</b>. The project is a proof of concept towards a mobile app that rewarded players with in-game currency whenever player's did any healthy activities, this includes exercise and meditation. After obtaining the currency, players can use it to pull for cute outfits and pets that they can decorate their avatar with.
+    ABOUT: `    PAWsitive was a hackathon submission for BCIT's QDS 2024 Hackathon, where the project was the <b>first place winner</b>. The project is a proof of concept towards a mobile app that rewarded players with in-game currency whenever player's did any healthy activities, this includes exercise and meditation. After obtaining the currency, players can use it to pull for cute outfits and pets that they can decorate their avatar with.
 
     The initial concept came from the idea that "gacha games" tend to have addictive elements to them that keeps their players hooked. We thought instead of using those addictive elements to exploit players, we should instead use them to build healthy habits in our players.
     `,
