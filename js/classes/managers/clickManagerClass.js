92c786b1e1638eafa3e554e6620e9cd90952fcc9
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-03-11 05:07:34 -0700

    add ability to interact via click on interactable objects

--- js/classes/managers/clickManagerClass.js
+++ js/classes/managers/clickManagerClass.js
@@ -14,15 +14,16 @@ class ClickManager {
         this.#camera = camera;
     }
 
-    getClickPosition(mouseEvent) {
-
+    // Expected to be called before getClickPosition and interactableClicked
+    setMouseRaycast(mouseEvent) {
         this.#pointPosition.x = (mouseEvent.clientX / window.innerWidth) * 2 - 1;
         this.#pointPosition.y = -(mouseEvent.clientY / window.innerHeight) * 2 + 1;
-
         this.#raycast.setFromCamera(this.#pointPosition, this.#camera);
+    }
 
+    getClickPosition() {
         // Check second intersect since first will be the invisible wall of the arcade
-        const intersects = this.#raycast.intersectObjects(this.#arcadeScene.children)
+        const intersects = this.#raycast.intersectObjects(this.#arcadeScene.children);
         let firstIntersectPoint;
         for (let i = 0; i < intersects.length; i++) {
             firstIntersectPoint = intersects[i].point; // In case click was out of bounds, still want to register some place
@@ -30,5 +31,15 @@ class ClickManager {
         }
         if (!firstIntersectPoint) return null;
         else return new THREE.Vector3(firstIntersectPoint.x, 0, firstIntersectPoint.z); // Removes elevation
+    }
+
+    interactableClicked() {
+        const intersects = this.#raycast.intersectObjects(this.#arcadeScene.children)
+        for(let i = 0; i < intersects.length; i++) {
+            if (intersects[i].object.type !== "Mesh") continue;
+            // (%ignore) is the clerk for some reason, don't ask me why it turned out like this
+            if (intersects[i].object.name.includes("arcade") || intersects[i].object.name.includes("(%ignore)")) return true;
+        }
+        return false;
     }
 }
