e8e988f9d726a1e8bb5b7bb873bae264028ed228
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-29 18:37:23 -0700

    fix bug where clicking on out of bounds causes movement jitters

--- js/classes/managers/clickManagerClass.js
+++ js/classes/managers/clickManagerClass.js
@@ -20,16 +20,17 @@ class ClickManager {
         this.#pointPosition.y = -(mouseEvent.clientY / window.innerHeight) * 2 + 1;
         this.#raycast.setFromCamera(this.#pointPosition, this.#camera);
     }
-
+    // TODO: FIX THE THING CLICKING ON AN OBJECT IT WILL KEEP WALKING IN PLACE, FIX IT TO STOP DOING THAT
     getClickPosition() {
         // Check second intersect since first will be the invisible wall of the arcade
         const intersects = this.#raycast.intersectObjects(this.#arcadeScene.children);
         let firstIntersectPoint;
         for (let i = 0; i < intersects.length; i++) {
-            firstIntersectPoint = intersects[i].point; // In case click was out of bounds, still want to register some place
-            if (intersects[i].object.type === "Mesh") break; // Click was in-bound, take the first thing it raycasted to
+            if (intersects[i].object.type !== "Mesh") continue;
+            firstIntersectPoint = intersects[i].point;
+            break;
         }
-        if (!firstIntersectPoint) return null;
+        if (!firstIntersectPoint) return this.#calculatePointAtYZero();
         else return new THREE.Vector3(firstIntersectPoint.x, 0, firstIntersectPoint.z); // Removes elevation
     }
 
@@ -41,5 +42,18 @@ class ClickManager {
             if (intersects[i].object.name.includes("arcade") || intersects[i].object.name.includes("(%ignore)")) return true;
         }
         return false;
+    }
+
+    // Gets the point in the line where Y = 0
+    #calculatePointAtYZero() {
+        const origin = this.#raycast.ray.origin;
+        const direction = this.#raycast.ray.direction;
+        // Prevent divide by 0 errors
+        if (direction.y !== 0) {
+            // P(t) = origin + direction * t (i.e. y = mx + b, the slope formula), solve for t when P(t) = 0
+            const t = -origin.y / direction.y;
+            return direction.clone().multiplyScalar(t).add(origin);
+        }
+        return null;
     }
 }
