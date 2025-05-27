450730930c50aca6c6e48149fd4a36ec726212a6
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-05-26 17:08:36 -0700

    implement load screen interaction logic

--- js/classes/visuals/loadScreenManagerClass.js
+++ js/classes/visuals/loadScreenManagerClass.js
@@ -1,21 +1,37 @@
 export { LoadScreenManager }
 
-// TODO: Implement logic for load screen
 class LoadScreenManager {
 
-    #inputManager;
+    // Html elements
+    #siteTitle;
     #loadScreen;
+    #loadMessage;
+    #loadIcon;
     #enterButton;
 
+    #inputManager;
+
     constructor(inputManager) {
+        this.#siteTitle = document.getElementById("site-title");
         this.#loadScreen = document.getElementById("load-screen");
+        this.#loadMessage = document.getElementById("load-message");
+        this.#loadIcon = document.getElementById("load-icon");
         this.#enterButton = document.getElementById("enter-button");
 
         this.#inputManager = inputManager;
         this.#inputManager.pauseInput(true);
+        this.#enterButton.addEventListener("click", () => this.#closeLoadScreen());
     }
 
     showButton() {
+        this.#siteTitle.style.display = "block";
+        this.#enterButton.style.display = "block";
+        this.#loadMessage.style.display = "none";
+        this.#loadIcon.style.display = "none";
+    }
 
+    #closeLoadScreen() {
+        this.#loadScreen.style.display = "none";
+        this.#inputManager.pauseInput(false);
     }
 }
